5a53b34d169c0e80d7d1b0733bb8d961ae568262
blackbret94 <emomartian@gmail.com>
2016-05-04 01:23:58 -0400

    Page file expanded

--- Page.java
+++ Page.java
@@ -1,3 +1,32 @@
 public class Page{
-
+  private boolean dirty;
+  private int birthday, lastUsed, nextUse;
+  
+  public Page(){
+    
+  }
+  
+  public int getBirthday(){
+    return birthday;
+  }
+  
+  public void setBirthday(int birthday){
+    this.birthday = birthday;  
+  }
+  
+  public int getLastUsed(){
+    return lastUsed;
+  }
+  
+  public void setLastUsed(int lastUsed){
+    this.lastUsed = lastUsed;
+  }
+  
+  public int getNextUse(){
+    return nextUse;
+  }
+  
+  public void setNextUse(int nextUse){
+    this.nextUse = nextUse;
+  }
 }
