b34c8f914ddfbc4e9aacb33108bc49584accc291
lomethoron <wdixon23192@gmail.com>
2016-05-03 23:33:37 -0400

    added updateTick to PageTable

--- PageTable.java
+++ PageTable.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 public class PageTable{
 	// Iterating over a hash map is a pita, gonna do it this way for less nightmare
 	LinkedList<Process> table;
-	int entries, pageSize;
+	int entries, pageSize, time;
+	PriorityQueue<int> kickNext;
 	
 	public PageTable(int entries, int pageSize, String alg){
 		this.entries = entries;
 		this.pageSize = pageSize;
 		table = new LinkedList<Process> (entries);
+		kickNext = new PriorityQueue
 		
 	}
 	
@@ -26,6 +30,10 @@ public class PageTable{
 	// keeps all the prints in the Main class
 	public boolean add(Process){
 		return true;
+	}
+	
+	public void updateTick(int time){
+		this.time = time;
 	}
 
 }
